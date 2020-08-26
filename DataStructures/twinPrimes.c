39eb91e277d6750cdba91dba00ac9289b536d3f0
roshanajicherian <roshanaji777@gmail.com>
2020-08-26 07:58:23 +0530

    Improved time complexity

--- DataStructures/twinPrimes.c
+++ DataStructures/twinPrimes.c
@@ -2,7 +2,8 @@
 #include <stdbool.h>
 bool isPrime(int n)
 {
-    for (int i = 2; i < n; i++)
+    int i=0;
+    for (i = 2; i*i <= n; i++)
     {
         if (n % i == 0)
             return false;
@@ -11,8 +12,9 @@ bool isPrime(int n)
 }
 int main()
 {
+    int i=0;
     printf("The twin primes till 100 are : \n");
-    for (int i = 3; i <= 100; i++)
+    for (i = 3; i <= 100; i++)
     {
         if ((i + 2 <= 100) && (isPrime(i)) && (isPrime(i + 2)))
             printf("(%d,%d)\n", i, i + 2);
