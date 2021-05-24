4c538dbc77224742a513614d6e1a3bea0b8bf456
Roshan Aji Cherian <roshanaji777@gmail.com>
2021-05-24 19:10:24 +0530

    Added 3 programs

---
+++ ObjectOrientedPrograming/pgm1.java
@@ -0,0 +1,12 @@
+public class pgm1{
+    //  Salil      salil@cse          p03160                       25764728
+    public static void main(String[] args) {
+        System.out.println("Name\tMailId\t\tEmployee Code\tPhone");
+        System.out.println("Umesh\tumesh@cse\tp03161\t\t25764728");        
+        System.out.println("Salil\tsalil@cse\tp03160\t\t25764728");        
+        System.out.println("John\tjohn@cse\tp03159\t\t25754728");        
+        System.out.println("Doe\tdoe@cse\t\tp03168\t\t22764728");        
+        System.out.println("Jane\tjane@cse\tp03163\t\t15764728");        
+        
+    }
+}
---
+++ ObjectOrientedPrograming/pgm3.java
@@ -0,0 +1,13 @@
+public class pgm3 {
+    public static void main(String[] args) {
+        int a_number=1;
+        if(a_number==1)
+            System.out.println("One");
+        else if(a_number==2)
+            System.out.println("Two");
+        else if(a_number==3)
+            System.out.println("Three");
+        else if(a_number==4)
+            System.out.println("Four");
+    }
+}
