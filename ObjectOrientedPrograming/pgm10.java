6291e2c7e91a0a25e5ee3b924498830403bb6247
Roshan Aji Cherian <roshanaji777@gmail.com>
2021-09-11 08:30:05 +0530

    Added pgm10.java

---
+++ ObjectOrientedPrograming/pgm10.java
@@ -0,0 +1,29 @@
+class Box
+{
+    String itemid;
+    Box(String itemid)
+    {
+        this.itemid=itemid;
+    }
+}
+class BoxWeight extends Box
+{
+    int weight;
+    BoxWeight(String itemid,int weight)
+    {
+        super(itemid);
+        this.weight=weight;
+    }
+}
+class Shipment extends BoxWeight
+{
+    int cost;
+    Shipment(String itemid,int weight,int cost)
+    {
+        super(itemid,weight);
+        this.cost=cost;
+    }
+}
+public class pgm10 {
+    Shipment newpackage=new Shipment("ABC01", 100, 100);
+}
